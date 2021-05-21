8533c07b814a6801dc003f4281bae017bdcb3953
Matt Benton <matt.bentonn@hotmail.com>
2021-05-22 00:42:43 +1000

    Revert "Delete YoutubeConfig.java"
    This reverts commit ff89460665d88a25d08dd6def4d7165677b77ed9.
    
    reverting the deletion since you can still see old commits anyway (duh)

---
+++ app/src/main/java/com/example/youtubeplayer/YoutubeConfig.java
@@ -0,0 +1,14 @@
+package com.example.youtubeplayer;
+
+public class YoutubeConfig {
+
+    public YoutubeConfig(){
+    }
+
+    private static final String API_KEY = "AIzaSyDLIfQdRyRrJPq_DPeEGGKd9SNxwYM497A";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+
+}
